8549dbbde9b9dce05f7a335329b684b78e4e8ce5
Brandon Lyman <lyman.br@northeastern.edu>
2022-07-03 16:59:35 -0400

    Enemy Spawn Behaviour
    Enemies now spawn based on player score and a coefficient. Multiply the coefficient by the players score and round to an integer to get the amount of enemies spawned per "wave." This behaviour is naive and unbalanced, but the result feels surprisingly like the game being cloned.

--- ShooterCrateBoxProject/Assets/Scripts/Spawners/EnemySpawner.cs
+++ ShooterCrateBoxProject/Assets/Scripts/Spawners/EnemySpawner.cs
@@ -7,10 +7,66 @@ using UnityEngine;
 /// </summary>
 public class EnemySpawner : MonoBehaviour
 {
+    /// <summary>
+    /// Multiply player's score by this to calculate how many enemies are 
+    /// spawned in a wave.
+    /// </summary>
+    [Tooltip("Multiply player's score by this to calculate how many " +
+        "enemies are spawned in a wave.")]
+    [SerializeField] private float enemyCounterCoefficient = 0.5f;
     [SerializeField] private RollerEnemyPool rollerEnemyPool;
     [SerializeField] private Transform enemySpawnPoint;
+    [SerializeField] private Vector2Int intrawaveWaitTime;
+    [SerializeField] private Vector2Int interwaveWaitTime;
+    [SerializeField] private IntVariable playerScore;
 
-    public void SpawnRollerEnemy()
+    private IEnumerator currentCoroutine;
+
+    public void OnGameStart()
+    {
+        SpawnRollerEnemy();
+        StartSpawnCycle();
+    }
+
+    public void OnGameEnd()
+    {
+        StopAllCoroutines();
+    }
+
+    private void StartSpawnCycle()
+    {
+        if (currentCoroutine != null)
+        {
+            StopCoroutine(currentCoroutine);
+        }
+        currentCoroutine = SpawnCycleRoutine();
+        StartCoroutine(currentCoroutine);
+    }
+
+    private IEnumerator SpawnCycleRoutine()
+    {
+        int timeBetweenWaves = Random.Range(interwaveWaitTime.x,
+            interwaveWaitTime.y);
+
+        yield return new WaitForSeconds(timeBetweenWaves);
+
+        int enemiesToSpawn = Mathf.RoundToInt(Mathf.Max(2, playerScore.Value *
+            enemyCounterCoefficient));
+        Debug.Log(enemiesToSpawn);
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            SpawnRollerEnemy();
+
+            int timeBetweenEnemies = Random.Range(intrawaveWaitTime.x,
+                intrawaveWaitTime.y);
+
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+
+        StartSpawnCycle();
+    }
+
+    private void SpawnRollerEnemy()
     {
         GameObject enemyObject = rollerEnemyPool.Pool.Get();
 
